f7d069c8d5199c226840a7b4ad899b719969a3c5
吴令 <wx_da9ca5d160264260a28c6febefd0c909@git.code.tencent.com>
2019-08-01 15:47:37 +0800

    '修改init_tool内容'

--- init_tool.py
+++ init_tool.py
@@ -1,75 +1,92 @@
 # -*- coding:utf-8 -*-
 # Author : 小吴老师
 # Data ：2019/7/18 20:27
-from tools import init_env
-from tools import init_project
-from tools import os_tool
-from tools import request_tool
-from tools import yaml_tool
-from tools import zip_tool
-root_path = os_tool.get_root_path()
-################################
-# 初始化环境：第三方框架+果芽工具包
-################################
-# 初始化环境(必须最先执行)
-def _init_env():
-    init_env.init_api_env()
+import os
+import shutil
+import stat
+import subprocess
+import yaml
 
-#########################################
-# 初始化工程：目录+配置+脚本
-#########################################
-def _init_api_project():
-    # 初始化工程目录
-    init_project.init_dirs()
-    # 　初始化配置文件
-    init_project.init_config()
-    # 初始化allure
-    init_project.ini_allure()
-    # 初始化演示demo
-    init_project.init_test_demo()
-    # 初始化excel模板
-    init_project.init_data()
-    # 初始化conftest模板
-    init_project.init_conftest()
-    # 初始化run.py
-    init_project.ini_run()
-    # 初始化git
-    init_project.init_git()
+def get_yaml(yaml_path):
+    with open(yaml_path,'r',encoding='utf-8') as f:
+        content =yaml.load(f.read(),Loader=yaml.FullLoader)
+    return content
 
-def init_api_project():
-    _init_env()
-    _init_api_project()
+def invoke(cmd):
+    try:
+        output, errors = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        o = output.decode("utf-8")
+        print(o)
+        return o
+    except Exception as e:
+        print('执行命令失败，请检查环境配置')
+        print(e)
+        raise
+
+def copy_file(src_file,target_dir):
+    shutil.copy(src_file,target_dir)
+
+def copy_dir(src_dir,target_dir):
+    if not os.path.exists(target_dir):
+        shutil.copytree(src_dir,target_dir)
+
+def get_root_path():
+    root_path = os.path.abspath(os.path.dirname(__file__)).replace('\\', '/')
+    print(root_path)
+    if root_path.find('venv') > 0:
+        root_path=root_path[:root_path.find('venv')-1]
+    return root_path+'/'
+
+def mkdir(path):
+    is_exists = os.path.exists(path)
+    if not is_exists:
+        os.makedirs(path)
+def deldir(dir):
+    if os.path.exists(dir):
+        for file in os.listdir(dir):
+            file = os.path.join(dir, file)
+            if os.path.isdir(file):
+                print("remove dir", file)
+                os.chmod(file, stat.S_IWRITE|stat.S_IWOTH)
+                deldir(file)
+            elif os.path.isfile(file) :
+                print("remove file", file)
+                os.chmod(file, stat.S_IWRITE|stat.S_IWOTH)
+                os.remove(file)
+        shutil.rmtree(dir,True)
+
+def copy_file(src_file,target_dir):
+    shutil.copy(src_file,target_dir)
+
 
 def init_ui_project():
+    root_path = get_root_path()
     # -*- coding:utf-8 -*-
     # Author : 小吴老师
     # Data ：2019/8/1 11:41
-    from tools import shell_tool, yaml_tool
-    from tools import os_tool
 
     ## 先删除，再下载tools和ui工程
     tools_prj = 'c:/guoya/auto_test_init/guoya-tools'
     ui_prj = 'c:/guoya/auto_test_init/guoya-ui-test'
-    os_tool.deldir(tools_prj)
-    os_tool.deldir(ui_prj)
-    os_tool.mkdir(tools_prj)
-    os_tool.mkdir(ui_prj)
+    deldir(tools_prj)
+    deldir(ui_prj)
+    mkdir(tools_prj)
+    mkdir(ui_prj)
     cmd = 'git clone https://github.com/LudvikWoo/guoya-tools.git ' + tools_prj
-    shell_tool.invoke(cmd)
+    invoke(cmd)
     cmd = 'git clone https://github.com/LudvikWoo/guoya-ui-test.git ' + ui_prj
-    shell_tool.invoke(cmd)
+    invoke(cmd)
 
     ## 读取ui初始化配置文件
-    y = yaml_tool.get_yaml(ui_prj + '/init_ui_project.yaml')
+    y = get_yaml(ui_prj + '/init_ui_project.yaml')
     ## 复制文件夹
     dirs = y['dirs']
-    root_path = os_tool.get_root_path()
     for dir in dirs:
-        os_tool.copy_dir(ui_prj + '/' + dir, root_path + '/' + dir)
+        copy_dir(ui_prj + '/' + dir, root_path + dir)
     ## 复制文件
     files = y['files']
     for file in files:
-        os_tool.copy_file(ui_prj + '/' + file, root_path)
+        copy_file(ui_prj + '/' + file, root_path)
 
 if __name__ == '__main__':
     init_ui_project()
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name="guoya-tools",  # 这里是pip项目发布的名称
-    version="1.1.1",  # 版本号，数值大的会优先被pip
+    version="1.1.3",  # 版本号，数值大的会优先被pip
     keywords=["init", "auto-test"],
     description="to simplify auto test",
     long_description="A init package,to simplify develope auto test",
@@ -17,6 +17,7 @@ setup(
     author="wuling",
     author_email="wuling@guoyasoft.com",
     data_files =['init_tool.py'],
+    packages=['tools'],
     platforms="python",
     install_requires=[
         'pinyin==0.4.0',
