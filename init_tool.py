5d665d53f4e903ac7fde513c2bf30924cc2042cb
吴令 <wx_da9ca5d160264260a28c6febefd0c909@git.code.tencent.com>
2019-08-04 09:48:26 +0800

    '更新初始化方法'

--- init_tool.py
+++ init_tool.py
@@ -89,9 +89,9 @@ def down_big_file(srcUrl, localFile):
 
 def init_ui_project():
     root_path = get_root_path()
-    url = 'http://chromedriver.storage.googleapis.com/75.0.3770.90/chromedriver_win32.zip'
-    mkdir(root_path + 'chrome_driver')
-    down_big_file(url,root_path+'chrome_driver/chromedriver.exe')
+    # url = 'http://chromedriver.storage.googleapis.com/75.0.3770.90/chromedriver_win32.zip'
+    # mkdir(root_path + 'chrome_driver')
+    # down_big_file(url,root_path+'chrome_driver/chromedriver.exe')
 
     ## 先删除，再下载tools和ui工程
     tools_prj = 'c:/guoya/auto_test_init/guoya-tools'
